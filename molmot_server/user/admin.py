6b7a6314f5300553699eb8f3e2da25fdc195ff8f
heejeong <0120_h@naver.com>
2022-03-26 17:11:35 +0900

    uuid -> pk 기본 유저 모델 정리

--- molmot_server/user/admin.py
+++ molmot_server/user/admin.py
@@ -1,17 +1,3 @@
 from user import models
 from django.contrib import admin
 
-# Register your models here.
-@admin.register(models.User)
-class User(admin.ModelAdmin):
-     list_display= (
-    'email',
-    'password',
-     'username',
-     'birth',
-     'age',
-     'gender',
-     'date_joined',
-    'last_login',
-    'is_active'
-     )
