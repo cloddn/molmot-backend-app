4423ba24297873526585cb03e37aef1f746562a8
heejeong <0120_h@naver.com>
2022-03-31 14:52:57 +0900

    아이디 및 비밀번호 찾기

--- molmot_server/user/urls.py
+++ molmot_server/user/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('login', Login.as_view()),
     path('auth/',AuthSMSAPI.as_view()),
     path('auth/?phone_number=<str:phone_number>&auth_number=<int:auth_number>',AuthSMSAPI.as_view()),
+    path('idpw-check/',IDPWCheckingAPI.as_view()),
     path('idpw-check/?phone_number=<str:phone_number>&auth_number=<int:auth_number>',IDPWCheckingAPI.as_view()),
 
 ]
