75949c0704c0de66bf14739a8248fcb1f3c15507
heejeong <0120_h@naver.com>
2022-04-30 17:49:58 +0900

    django admin

--- molmot_server/media/admin.py
+++ molmot_server/media/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from media import models
+
 # Register your models here.
+
+@admin.register(models.UIPhoto)
+class UIPhotoAdmin(admin.ModelAdmin):
+    list_display = (
+        'field',
+        'uuid',
+        'indexnum',
+        'photo_file'
+    )
