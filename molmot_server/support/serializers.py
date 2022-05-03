770ef61a4405bd305dde9c340b0db40b65c8811c
heejeong <0120_h@naver.com>
2022-05-03 21:39:05 +0900

    data 받아올 데이터 없을 경우 에러 처리

--- molmot_server/support/serializers.py
+++ molmot_server/support/serializers.py
@@ -115,8 +115,7 @@ class SupportBookMarkSerializer(serializers.ModelSerializer):
         return data
 
     def get_d_day(self,data):
-        support_id=Support.objects.get(title=data['support_id'])
-        d_day=str((support_id.end_date.date()-datetime.date.today()).days)
+        d_day=str((data.end_date.date()-datetime.date.today()).days)
         return d_day
 
 '''
