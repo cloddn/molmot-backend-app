9b093a63fd85ea18467ef508902cb449011cd594
heejeong <0120_h@naver.com>
2022-05-08 14:57:12 +0900

    post_ save signals 해제

--- molmot_server/support/signals.py
+++ molmot_server/support/signals.py
@@ -13,14 +13,14 @@ from user.models import MemberFCMDevice
 @receiver(signals.post_save, sender=Support) #Support 정보 수정하면 -> SupportNotification 수정하기.
 def run_task_on_Support_save(sender, instance, created, **kwargs):
     if (created==False):
-        try:
+        #try:
             #신청 날짜 지나면 마감 하루 일자 하루전 , 안지났으면 시작 일자 7일 동안 울리게 
-            sched_noti=SupportNotification.objects.filter(pk=instance.pk).update(
-                    noti_on_time=instance.start_date,
-                    crontab=instance.start_date
-                    )
-        except SupportNotification.DoesNotExist:
-            pass
+        #    sched_noti=SupportNotification.objects.filter(pk=instance.pk).update(
+        #            noti_on_time=instance.start_date,
+        #            crontab=instance.start_date
+        #            )
+        #except SupportNotification.DoesNotExist:
+        #    pass
 
 
 #즐겨찾기 수정됐을때가 있나....?즐겨찾기 삭제될경우 -> 없어져야함.
