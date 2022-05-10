d025f9b0e91a265ffb5509aa8105fb9514f7eaab
heejeong <0120_h@naver.com>
2022-05-10 16:49:31 +0900

    USE_TZ = True

--- molmot_server/molmot_server/settings.py
+++ molmot_server/molmot_server/settings.py
@@ -175,7 +175,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = False  # False 로 설정해야 DB에 변경 된 TIME_ZONE 이 반영 됨
+USE_TZ = True  # False 로 설정해야 DB에 변경 된 TIME_ZONE 이 반영 됨
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
