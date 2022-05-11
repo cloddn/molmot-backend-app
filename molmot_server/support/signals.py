2f7f6b6181c17f3e76e0cae3b776cb7d17e582ee
heejeong <0120_h@naver.com>
2022-05-12 01:44:25 +0900

    알림 관리 api 로만

--- molmot_server/support/signals.py
+++ molmot_server/support/signals.py
@@ -24,13 +24,14 @@ from user.models import MemberFCMDevice
 
 
 #즐겨찾기 수정됐을때가 있나....?즐겨찾기 삭제될경우 -> 없어져야함.
+'''
 @receiver(signals.post_save, sender=SupportBookMark) #즐겨찾기한 지원금 제도에 한정해서 추가되도록 
 def run_task_on_SupportBookMark_save(sender, instance, created, **kwargs):
     if (created):
         try:
             #신청 날짜 지나면 마감 하루 일자 하루전 , 안지났으면 시작 일자 7일 동안 울리게 
             #기기 토큰 1개만 가지고 있을 수 있도록...?
-            schedule, is_created =CrontabSchedule.objects.get_or_create(
+            schedule =CrontabSchedule.objects.create(
             minute=00,
             hour=17,
             day_of_month=datetime.datetime.today().day,
@@ -54,7 +55,7 @@ def run_task_on_SupportBookMark_save(sender, instance, created, **kwargs):
         except Support.DoesNotExist or MemberFCMDevice.DoesNotExist:
             pass
    
-
+'''
 
 @receiver(signals.post_delete, sender=SupportBookMark) #즐겨찾기한 지원금 제도에 한정해서 추가되도록 
 def run_task_on_SupportBookMark_deleted_save(sender, instance, **kwargs):
