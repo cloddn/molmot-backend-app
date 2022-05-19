8a8f76072bf4c2f1a0e697cb2d3b89c98fe0f919
heejeong <0120_h@naver.com>
2022-05-20 00:10:12 +0900

    issue chromedriver install

--- molmot_server/support/views.py
+++ molmot_server/support/views.py
@@ -1,6 +1,6 @@
 
 import datetime
-
+from webdriver_manager.chrome import ChromeDriverManager
 import random
 
 from unicodedata import category
@@ -921,15 +921,21 @@ class GetSmartDetailRecommendView(APIView):
         soup = BeautifulSoup(website.text,"html.parser")
     
 
-        chrome_options = Options()
-        chrome_options.add_argument( '--headless' )
-        chrome_options.add_argument( '--log-level=3' )
-        chrome_options.add_argument( '--disable-logging' )
-        chrome_options.add_argument( '--no-sandbox' )
-        chrome_options.add_argument( '--disable-gpu' )
-        chrome_options.headless = True
-        
-        driver = webdriver.Chrome('/home/ubuntu/molmot-backend-app/molmot_server/chromedriver',chrome_options=chrome_options)
+        #chrome_options = Options()
+        #chrome_options.add_argument( '--headless' )
+        #chrome_options.add_argument( '--log-level=3' )
+        #chrome_options.add_argument( '--disable-logging' )
+        #chrome_options.add_argument( '--no-sandbox' )
+        #chrome_options.add_argument( '--disable-gpu' )
+
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        #chrome_options.headless = True
+        driver = webdriver.Chrome(ChromeDriverManager().install())
+
+        #driver = webdriver.Chrome('/home/ubuntu/molmot-backend-app/molmot_server/chromedriver',chrome_options=chrome_options)
         driver.implicitly_wait(3)
         
         driver.get( URL )
