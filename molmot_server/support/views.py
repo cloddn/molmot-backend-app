8d3b5466f7224c755b5b4097224a9ce09ad99494
heejeong <0120_h@naver.com>
2022-05-20 00:11:50 +0900

    issue -chromedriver

--- molmot_server/support/views.py
+++ molmot_server/support/views.py
@@ -933,9 +933,8 @@ class GetSmartDetailRecommendView(APIView):
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
         #chrome_options.headless = True
-        driver = webdriver.Chrome(ChromeDriverManager().install())
-
-        #driver = webdriver.Chrome('/home/ubuntu/molmot-backend-app/molmot_server/chromedriver',chrome_options=chrome_options)
+        driver = webdriver.Chrome(executable_path="/home/ubuntu/molmot-backend-app/molmot_server/chromedriver",chrome_options=chrome_options)
+        #driver = webdriver.Chrome(executable_path="/home/ubuntu/molmot-backend-app/molmot_server/chromedriver",chrome_options=chrome_options)
         driver.implicitly_wait(3)
         
         driver.get( URL )
